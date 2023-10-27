f9b17a653e9f147a70980df6888c25f83ae4aadd
TommyLike <tommylikehu@gmail.com>
2023-10-27 09:13:38 +0800

    Support codesign

--- src/infra/sign_plugin/x509.rs
+++ src/infra/sign_plugin/x509.rs
@@ -29,9 +29,7 @@ use openssl::pkcs7::{Pkcs7, Pkcs7Flags};
 use openssl::pkey::PKey;
 use openssl::stack::Stack;
 use openssl::x509;
-use openssl::x509::extension::{
-    AuthorityKeyIdentifier, BasicConstraints, KeyUsage, SubjectKeyIdentifier,
-};
+use openssl::x509::extension::{AuthorityKeyIdentifier, BasicConstraints, ExtendedKeyUsage, KeyUsage, SubjectKeyIdentifier};
 use openssl::x509::{X509Crl, X509Extension};
 use openssl_sys::{
     X509_CRL_add0_revoked, X509_CRL_new, X509_CRL_set1_lastUpdate, X509_CRL_set1_nextUpdate,
@@ -329,12 +327,13 @@ impl X509Plugin {
                 .critical()
                 .build()?,
         )?;
-        generator.append_extension(X509Extension::new_nid(
-            None,
-            None,
-            Nid::CRL_DISTRIBUTION_POINTS,
-            &self.generate_crl_endpoint(&self.parent_key.clone().unwrap().name, infra_config)?,
-        )?)?;
+        //NOTE: sbverify for EFI file will fail, enable when fixed
+        // generator.append_extension(X509Extension::new_nid(
+        //     None,
+        //     None,
+        //     Nid::CRL_DISTRIBUTION_POINTS,
+        //     &self.generate_crl_endpoint(&self.parent_key.clone().unwrap().name, infra_config)?,
+        // )?)?;
         generator.append_extension(X509Extension::new_nid(
             None,
             None,
@@ -432,12 +431,18 @@ impl X509Plugin {
                 .critical()
                 .build()?,
         )?;
-        generator.append_extension(X509Extension::new_nid(
-            None,
-            None,
-            Nid::CRL_DISTRIBUTION_POINTS,
-            &self.generate_crl_endpoint(&self.parent_key.clone().unwrap().name, infra_config)?,
-        )?)?;
+        generator.append_extension(
+            ExtendedKeyUsage::new()
+                .code_signing()
+                .build()?,
+        )?;
+        //NOTE: sbverify for EFI file will fail, enable when fixed
+        // generator.append_extension(X509Extension::new_nid(
+        //     None,
+        //     None,
+        //     Nid::CRL_DISTRIBUTION_POINTS,
+        //     &self.generate_crl_endpoint(&self.parent_key.clone().unwrap().name, infra_config)?,
+        // )?)?;
         generator.append_extension(X509Extension::new_nid(
             None,
             None,
